25e6502b65a2788ae1b9bed01249c375744fd9a1
Julian Gendritzki <jimbo.gend@icloud.com>
2025-12-11 10:34:59 +0100

    more animations for dec

--- player.py
+++ player.py
@@ -22,6 +22,22 @@ class Player(pygame.sprite.Sprite):
             image = pygame.transform.scale(image, (128, 128))
             self.run_frames.append(image)
 
+        forward_dir = Path(__file__).parent / "assets" / "DetectiveFromTheFutur" / "Forward"
+        self.forward_frames = []
+        for i in range(1, 7):
+            img_path = forward_dir / f"Forward_{i:02}.png"
+            image = pygame.image.load(str(img_path)).convert_alpha()
+            image = pygame.transform.scale(image, (128, 128))
+            self.forward_frames.append(image)
+        
+        backward_dir = Path(__file__).parent / "assets" / "DetectiveFromTheFutur" / "Backward"
+        self.backward_frames = []
+        for i in range(1, 7):
+            img_path = backward_dir / f"Backward_{i:02}.png"
+            image = pygame.image.load(str(img_path)).convert_alpha()
+            image = pygame.transform.scale(image, (128, 128))
+            self.backward_frames.append(image)       
+
         self.frame_index = 0
         self.image = self.idle_frames[self.frame_index]
 
@@ -44,14 +60,18 @@ class Player(pygame.sprite.Sprite):
     def update(self, keys, obstacles):
         moved = False
         left = False  # Flag für Linksbewegung
+        forward = False
+        backward = False
         dx, dy = 0, 0
 
         if keys[pygame.K_w]:
             dy = -self.speed
             moved = True
+            forward = True
         if keys[pygame.K_s]:
             dy = self.speed
             moved = True
+            backward = True
         if keys[pygame.K_a]:
             dx = -self.speed
             moved = True
@@ -82,4 +102,11 @@ class Player(pygame.sprite.Sprite):
                 frame = self.run_frames[self.frame_index]
                 if left:
                     frame = pygame.transform.flip(frame, True, False)
+                elif forward:
+                    self.frame_index = (self.frame_index +1) % len(self.forward_frames)
+                    frame = self.forward_frames[self.frame_index]
+                elif backward:
+                    self.frame_index = (self.frame_index +1) % len(self.backward_frames)
+                    frame = self.backward_frames[self.frame_index]
                 self.image = frame
+                
